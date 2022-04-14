7001c698fca09a856339ad583a42e0d09656cad7
Tiffani Patterson <71092406+tpatter1981@users.noreply.github.com>
2022-04-14 17:24:34 -0400

    Add files via upload

---
+++ API2/main.js
@@ -0,0 +1,10 @@
+getMainQuote();
+
+getRandomQuote('#random-quote', 1);
+getRandomQuote('#random-multiple-quotes', 5);
+
+window.dataLayer = window.dataLayer ||[];
+function gtag(){dataLa.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-023S8YNFSG');
